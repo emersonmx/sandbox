b4a95624ed49cf39f5efd446ae2f16ebc6f634ee
Emerson MX <emersonmx@gmail.com>
2022-02-05 15:47:18 -0300

    Add bevy hello_empty

---
+++ rust/bevy/hello_empty/src/main.rs
@@ -0,0 +1,16 @@
+use bevy::{prelude::*, input::system::exit_on_esc_system};
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(WindowDescriptor {
+            title: "Hello Empty".to_string(),
+            width: 640.0,
+            height: 480.0,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_system(exit_on_esc_system)
+        .run();
+}
