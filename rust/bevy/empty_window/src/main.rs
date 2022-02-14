144aee823b88a6019a4cafc174b85f80add6aed5
Emerson Max de Medeiros Silva <emerson.silva@invillia.com>
2022-02-14 11:18:55 -0300

    Update window title

--- rust/bevy/empty_window/src/main.rs
+++ rust/bevy/empty_window/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(WindowDescriptor {
-            title: "Hello Empty".to_string(),
+            title: "Empty Window".to_string(),
             width: 640.0,
             height: 480.0,
             resizable: false,
--- rust/bevy/move_square/src/main.rs
+++ rust/bevy/move_square/src/main.rs
@@ -51,7 +51,7 @@ fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .insert_resource(WindowDescriptor {
-            title: "Hello Square".to_string(),
+            title: "Move Square".to_string(),
             width: 640.0,
             height: 480.0,
             resizable: false,
