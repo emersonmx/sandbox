c8a70323ca1522c3551225a4870ef2844b9dc31d
Emerson MX <emersonmx@gmail.com>
2022-03-05 08:57:58 -0300

    [bevy] Simplify simple_mouse_grab

--- rust/bevy/simple_mouse_grab/src/main.rs
+++ rust/bevy/simple_mouse_grab/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{input::system::exit_on_esc_system, prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
@@ -11,39 +11,23 @@ fn main() {
             vsync: true,
             ..Default::default()
         })
-        .init_resource::<CursorState>()
         .add_plugins(DefaultPlugins)
         .add_system(toggle_grab)
-        .add_system(exit_on_esc_system)
         .run();
 }
 
-struct CursorState {
-    visible: bool,
-    grab: bool,
-}
-
-impl Default for CursorState {
-    fn default() -> Self {
-        Self {
-            visible: true,
-            grab: false,
-        }
-    }
-}
-
 fn toggle_grab(
-    input: Res<Input<KeyCode>>,
     mut windows: ResMut<Windows>,
-    mut cursor_state: ResMut<CursorState>,
+    mouse: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
 ) {
     let window = windows.get_primary_mut().unwrap();
-    if input.just_pressed(KeyCode::V) {
-        cursor_state.visible = !cursor_state.visible;
-        window.set_cursor_visibility(cursor_state.visible);
+    if mouse.just_pressed(MouseButton::Left) {
+        window.set_cursor_visibility(false);
+        window.set_cursor_lock_mode(true);
     }
-    if input.just_pressed(KeyCode::G) {
-        cursor_state.grab = !cursor_state.grab;
-        window.set_cursor_lock_mode(cursor_state.grab);
+    if key.just_pressed(KeyCode::Escape) {
+        window.set_cursor_visibility(true);
+        window.set_cursor_lock_mode(false);
     }
 }
