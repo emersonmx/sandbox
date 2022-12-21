fa3491286a0198d0ec18e132603430e4c969fe27
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-21 20:53:43 -0300

    [c/tcpl2nd] Fix main function returns

--- c/the_c_programming_language_2nd/exercise_1_2.c
+++ c/the_c_programming_language_2nd/exercise_1_2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-main()
+int main()
 {
     printf("hello, world\a"); // bell
     printf("hello, world\b"); // backspace
     printf("\033[31;1;4mhello, world\033[0m"); // with colors! :D
+    return 0;
 }
