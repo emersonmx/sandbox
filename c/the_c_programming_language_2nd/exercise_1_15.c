e983e864f1227fb44dc4aa9107f79767abfa2f6d
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-24 21:21:05 -0300

    [c/tcpl2nd] Add section 1.7 code

---
+++ c/the_c_programming_language_2nd/exercise_1_15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float fahrenheit_to_celsius(float value);
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = fahrenheit_to_celsius(fahr);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
+
+float fahrenheit_to_celsius(float value)
+{
+    return (5.0 / 9.0) * (value - 32.0);
+}
