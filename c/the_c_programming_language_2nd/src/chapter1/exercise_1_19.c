cdff0f2c363ba7e6811c5165041d70e226edff20
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-25 17:05:23 -0300

    [c/tcpl2nd] Add exercise text

--- c/the_c_programming_language_2nd/src/chapter1/exercise_1_19.c
+++ c/the_c_programming_language_2nd/src/chapter1/exercise_1_19.c
@@ -1,3 +1,6 @@
+// Write a function `reverse(s)` that reverses the character string `s`. Use it
+// to write a program that reverses its input a line at a time.
+
 #include <stdbool.h>
 #include <stdio.h>
 
