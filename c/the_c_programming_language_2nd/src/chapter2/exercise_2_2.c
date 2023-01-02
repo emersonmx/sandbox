5e2c72ed64d5cb55b19424d2b75cb7ce814d1cd9
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-01-02 16:03:25 -0300

    [c/tcpl2nd] Add section 2.6 code

---
+++ c/the_c_programming_language_2nd/src/chapter2/exercise_2_2.c
@@ -0,0 +1,27 @@
+// ```c
+// for (i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+//     s[i] = c;
+// ```
+//
+// Write a loop equivalent to the `for` loop above withou using `&&` or `||`.
+#include <stdio.h>
+
+int main()
+{
+    int  c;
+    int  lim = 20;
+    char s[lim];
+
+    for (int i = 0; i < lim - 1; ++i) {
+        c = getchar();
+        if (c == '\n')
+            break;
+        if (c == EOF)
+            break;
+        s[i] = c;
+    }
+
+    printf("%.*s\n", lim, s);
+
+    return 0;
+}
