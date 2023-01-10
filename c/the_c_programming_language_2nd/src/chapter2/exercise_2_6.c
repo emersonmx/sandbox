e3393ab8d042084d5c31cc4674ba8f52bab1265a
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-01-10 00:13:15 -0300

    [c/tcpl2nd] Add section 2.9 code

---
+++ c/the_c_programming_language_2nd/src/chapter2/exercise_2_6.c
@@ -0,0 +1,25 @@
+// Write a function `setbits(x, p, n, y)` that returns `x` with the `n` bits
+// that begin at position `p` set to the rightmost `n` bits of `y`, leaving the
+// other bits unchanged.
+
+#include <stdio.h>
+
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+
+int main()
+{
+    unsigned x = 255; // 11111111
+    unsigned y = 24; // 00011000
+    unsigned r = setbits(x, 5, 4, y); // 11011011
+
+    printf("%u\n", (unsigned)r);
+    return 0;
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    unsigned base = ~(~0 << n);
+    int      length = p + 1 - n;
+
+    return (x & ~(base << length)) | (y & base << length);
+}
---
+++ c/the_c_programming_language_2nd/src/chapter2/exercise_2_8.c
@@ -0,0 +1,32 @@
+// Write a function `rightrot(x, n)` that returns the value of the integer `x`
+// rotated to the right by `n` bit positions.
+
+#include <stdio.h>
+
+unsigned rightrot(unsigned x, int n);
+int      length(void);
+
+int main()
+{
+    unsigned x = 206; // 11001110
+    unsigned r = rightrot(x, 2); // 10110011
+
+    printf("%u\n", (unsigned)r);
+    return 0;
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+    unsigned rb = x << (length() - n);
+    return (x >> n) | rb;
+}
+
+int length(void)
+{
+    int      i;
+    unsigned l = (unsigned)~0;
+
+    for (i = 1; (l = l >> 1) > 0; ++i)
+        ;
+    return i;
+}
