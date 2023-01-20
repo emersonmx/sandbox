781e7a690ebef88c0fc5d1dab05092b8155c10f4
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-01-19 22:30:46 -0300

    [c/tcpl2nd] Add wip code for section 4.3

---
+++ c/the_c_programming_language_2nd/src/chapter4/wip_exercise_4_7.c
@@ -0,0 +1,8 @@
+// Write a routine `ungets(s)` that will push back an entire string onto the
+// input. Should `ungets` know about `buf` and `bufp`, or should it just use
+// `ungetch`?
+
+int main()
+{
+    return 0;
+}
---
+++ c/the_c_programming_language_2nd/src/chapter4/wip_exercise_4_9.c
@@ -0,0 +1,8 @@
+// Our `getch` and `ungetch` do not handle a pushed-back EOF correctly. Decide
+// what their properties ought to be if an `EOF` is pushed back, then implement
+// you design.
+
+int main()
+{
+    return 0;
+}
