04f0b46bad83ce73b00e78bfe91fc46a3ce71940
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-02-05 15:31:10 -0300

    [java/hyperskill] Update Cinema code

--- java/hyperskill/JavaForBeginners/Cinema.java
+++ java/hyperskill/JavaForBeginners/Cinema.java
@@ -2,55 +2,49 @@ import java.util.*;
 
 public class Cinema {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number of rows:");
-        System.out.print("> ");
-        int rows = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        System.out.print("> ");
-        int columns = scanner.nextInt();
+        int rows = getInput("Enter the number of rows:");
+        int columns = getInput("Enter the number of seats in each row:");
         System.out.println();
 
+        char[][] seats = createSeates(rows, columns);
+
+        boolean running = true;
+        while (running) {
+            int input = getInput("""
+                    1. Show the seats
+                    2. Buy a ticket
+                    0. Exit""");
+            switch (input) {
+                case 1:
+                    showSeats(seats, rows, columns);
+                    break;
+                case 2:
+                    buyTicket(seats, rows, columns);
+                    break;
+                case 0:
+                    running = false;
+                    break;
+            }
+        }
+    }
+
+    public static char[][] createSeates(int rows, int columns) {
         char[][] seats = new char[rows][columns];
         for (int i = 0; i < seats.length; i++) {
             Arrays.fill(seats[i], 'S');
         }
-
-        printSeats(seats);
-
-        System.out.println("Enter a row number:");
-        System.out.print("> ");
-        int row = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        System.out.print("> ");
-        int column = scanner.nextInt();
-
-        int price = 0;
-        int seatCount = rows * columns;
-        if (seatCount < 60) {
-            price = 10;
-        } else {
-            int frontHalf = rows / 2;
-            if (row <= frontHalf) {
-                price = 10;
-            } else {
-                price = 8;
-            }
-        }
-
-        System.out.println();
-        System.out.println("Ticket price: $" + price);
-        System.out.println();
-
-        seats[row - 1][column - 1] = 'B';
-        printSeats(seats);
+        return seats;
     }
 
-    public static void printSeats(char[][] seats) {
-        int rows = seats.length;
-        int columns = seats.length > 0 ? seats[0].length : 0;
+    public static int getInput(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        System.out.print("> ");
+        return scanner.nextInt();
+    }
 
+    public static void showSeats(char[][] seats, int rows, int columns) {
+        System.out.println();
         System.out.println("Cinema:");
         for (int i = 0; i <= columns; i++) {
             if (i == 0) {
@@ -68,6 +62,30 @@ public class Cinema {
             }
             System.out.println();
         }
+        System.out.println();
+    }
 
+    public static void buyTicket(char[][] seats, int rows, int columns) {
+        int row = getInput("Enter a row number:");
+        int column = getInput("Enter a seat number in that row:");
+        int price = calculateTicketPrice(row, column, rows, columns);
+        seats[row - 1][column - 1] = 'B';
+        System.out.println();
+        System.out.println("Ticket price: $" + price);
+        System.out.println();
+    }
+
+    public static int calculateTicketPrice(int row, int column, int rows, int columns) {
+        int seatCount = rows * columns;
+        if (seatCount < 60) {
+            return 10;
+        }
+
+        int frontHalf = rows / 2;
+        if (row <= frontHalf) {
+            return 10;
+        }
+
+        return 8;
     }
 }
