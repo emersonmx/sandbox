6f6612bfd850b276842329478c1322c62996239e
Emerson MX <emersonmx@gmail.com>
2023-10-19 10:11:27 -0300

    [python/djfast-polls] Add some comments

--- python/djfast-polls/djfast_polls/asgi.py
+++ python/djfast-polls/djfast_polls/asgi.py
@@ -13,6 +13,8 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djfast_polls.settings")
 
+# You must call django.setup() before using Django models. Function
+# get_asgi_application() already does this internally.
 django_app = get_asgi_application()
 
 
@@ -22,5 +24,6 @@ from api.routes import router as api_router
 
 app = FastAPI(title="djfast-polls")
 
+# The FastAPI's routes must come before django urls to avoid overlapping.
 app.include_router(api_router, prefix="/api")
 app.mount("/", django_app)
