61e34b42e40df36d245174f05825f1ee8b23c258
Emerson MX <emersonmx@gmail.com>
2024-01-29 17:45:47 -0300

    Migrate empty_window to bevy 0.12

--- rust/bevy/empty_window/src/main.rs
+++ rust/bevy/empty_window/src/main.rs
@@ -1,16 +1,17 @@
-use bevy::{input::system::exit_on_esc_system, prelude::*};
+use bevy::{prelude::*, window::close_on_esc};
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .insert_resource(WindowDescriptor {
-            title: "Empty Window".to_string(),
-            width: 640.0,
-            height: 480.0,
-            resizable: false,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_system(exit_on_esc_system)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Empty Window".to_owned(),
+                resolution: (640.0, 480.0).into(),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Update, close_on_esc)
         .run();
 }
