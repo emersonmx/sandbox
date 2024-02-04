77f20c84b94e132f9ea384fee71b40b9d1daa6a4
Emerson MX <emersonmx@gmail.com>
2024-02-04 11:38:53 -0300

    [rust/bevy] Remove workspace

--- rust/bevy/_scripts/runwin.sh
+++ rust/bevy/_scripts/runwin.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+pushd "$1"
+
 cargo build --target x86_64-pc-windows-gnu
 
-cp -vf "target/x86_64-pc-windows-gnu/debug/$1" .
-exec "./$1"
+cp -vf "target/x86_64-pc-windows-gnu/debug/$1.exe" .
+exec "./$1.exe"
