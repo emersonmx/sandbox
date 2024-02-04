99bc4bfb433ee4de372da029954c9f3e2116f13d
Emerson MX <emersonmx@gmail.com>
2024-02-04 13:09:59 -0300

    Update runwin script

--- rust/bevy/_scripts/runwin.sh
+++ rust/bevy/_scripts/runwin.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-cd "$1"
+(cd "$1"; cargo build --target x86_64-pc-windows-gnu)
 
-cargo build --target x86_64-pc-windows-gnu
-
-cp -vf "target/x86_64-pc-windows-gnu/debug/$1.exe" .
+cp -vf "$1/target/x86_64-pc-windows-gnu/debug/$1.exe" .
 exec "./$1.exe"
