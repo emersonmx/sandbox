e9b95def52877f71b980c643d5da3363ecc2b301
Emerson MX <emersonmx@gmail.com>
2024-08-04 14:10:05 -0300

    Add rust/simple2d (#10)
    * [rust] Add simple wgpu program
    
    * [rust/simple2d] Update code
    
    * [rust/simple2d] Update code

---
+++ rust/simple2d/scripts/runwin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+bin_name="simple2d.exe"
+project_name="simple2d"
+target_path="target/x86_64-pc-windows-gnu/debug"
+output_dir="$OUTPUT_BASE_PATH/$project_name"
+bin_path="$target_path/$bin_name"
+
+cargo build --target x86_64-pc-windows-gnu
+strip "$bin_path"
+
+rm -rf "$output_dir"
+mkdir -p "$output_dir"
+cp -vf "$bin_path" "$output_dir"
+cp -vf "./src/shader.wgsl" "$output_dir"
+
+(cd "$output_dir" && exec "$output_dir/$bin_name")
