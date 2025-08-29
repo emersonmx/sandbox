f2e74d6db31ce6429731acdfc210d3fd2e067bf4
Emerson MX <emersonmx@gmail.com>
2025-08-29 13:22:25 -0300

    c/void-arrays: break array into .h and .c

---
+++ c/void-arrays/array.c
@@ -0,0 +1,74 @@
+#include "array.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void array_init(Array *array, size_t item_size, size_t capacity)
+{
+    array->data = malloc(capacity * item_size);
+    array->item_size = item_size;
+    array->size = 0;
+    array->capacity = capacity;
+}
+
+void *array_get(Array *array, size_t index)
+{
+    if (index >= array->size)
+        return NULL;
+
+    return (char *)array->data + index * array->item_size;
+}
+
+void array_grow(Array *array, size_t new_capacity)
+{
+    if (new_capacity <= array->capacity)
+        return;
+
+    array->data = realloc(array->data, new_capacity * array->item_size);
+    array->capacity = new_capacity;
+}
+
+void array_insert(Array *array, size_t index, void *item)
+{
+    if (index > array->size)
+        return;
+
+    if (array->size >= array->capacity)
+        array_grow(array, array->capacity * 2);
+
+    void *target = (char *)array->data + index * array->item_size;
+    memmove((char *)target + array->item_size, target,
+            (array->size - index) * array->item_size);
+    memcpy(target, item, array->item_size);
+    array->size++;
+}
+
+void array_remove(Array *array, size_t index)
+{
+    if (index >= array->size)
+        return;
+
+    void *target = (char *)array->data + index * array->item_size;
+    memmove(target, (char *)target + array->item_size,
+            (array->size - index - 1) * array->item_size);
+    array->size--;
+}
+
+void array_push(Array *array, void *item)
+{
+    array_insert(array, array->size, item);
+}
+
+void array_pop(Array *array)
+{
+    if (array->size == 0)
+        return;
+
+    array->size--;
+}
+
+void array_free(Array *array)
+{
+    free(array->data);
+    *array = (Array){ 0 };
+}
---
+++ c/void-arrays/array.h
@@ -0,0 +1,22 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <stddef.h>
+
+typedef struct {
+    void *data;
+    size_t size;
+    size_t item_size;
+    size_t capacity;
+} Array;
+
+void array_init(Array *array, size_t item_size, size_t capacity);
+void *array_get(Array *array, size_t index);
+void array_grow(Array *array, size_t new_capacity);
+void array_insert(Array *array, size_t index, void *item);
+void array_remove(Array *array, size_t index);
+void array_push(Array *array, void *item);
+void array_pop(Array *array);
+void array_free(Array *array);
+
+#endif // ARRAY_H
