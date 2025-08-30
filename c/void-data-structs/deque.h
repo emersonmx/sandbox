a42a37e68fc6ff8f91928742d9f6b6912155af45
Emerson MX <emersonmx@gmail.com>
2025-08-30 16:46:40 -0300

    c/void-data-structs: add deque data struct

---
+++ c/void-data-structs/deque.h
@@ -0,0 +1,24 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <stddef.h>
+
+typedef struct {
+    void *data;
+    size_t size;
+    size_t item_size;
+    size_t capacity;
+    size_t front;
+    size_t back;
+} Deque;
+
+void deque_init(Deque *deque, size_t item_size, size_t capacity);
+void *deque_get(Deque *deque, size_t index);
+void deque_grow(Deque *deque, size_t new_capacity);
+void deque_push_front(Deque *deque, void *item);
+void deque_push_back(Deque *deque, void *item);
+void deque_pop_front(Deque *deque);
+void deque_pop_back(Deque *deque);
+void deque_free(Deque *deque);
+
+#endif // DEQUE_H
