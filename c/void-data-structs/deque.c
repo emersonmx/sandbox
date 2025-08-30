b40efdd7c251ee0f5889d72412743517fd524ea1
Emerson MX <emersonmx@gmail.com>
2025-08-30 16:46:40 -0300

    c/void-data-structs: add deque data struct

---
+++ c/void-data-structs/deque.c
@@ -0,0 +1,89 @@
+#include "deque.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void deque_init(Deque *deque, size_t item_size, size_t capacity)
+{
+    deque->data = malloc(capacity * item_size);
+    deque->item_size = item_size;
+    deque->size = 0;
+    deque->capacity = capacity;
+    deque->front = 0;
+    deque->back = 0;
+}
+
+void *deque_get(Deque *deque, size_t index)
+{
+    if (index >= deque->size)
+        return NULL;
+
+    size_t real_index = (deque->front + index) % deque->capacity;
+    return (char *)deque->data + real_index * deque->item_size;
+}
+
+void deque_grow(Deque *deque, size_t new_capacity)
+{
+    if (new_capacity <= deque->capacity)
+        return;
+
+    void *new_data = malloc(new_capacity * deque->item_size);
+    for (size_t i = 0; i < deque->size; i++) {
+        size_t real_index = (deque->front + i) % deque->capacity;
+        memcpy((char *)new_data + i * deque->item_size,
+               (char *)deque->data + real_index * deque->item_size,
+               deque->item_size);
+    }
+
+    free(deque->data);
+    deque->data = new_data;
+    deque->capacity = new_capacity;
+    deque->front = 0;
+    deque->back = deque->size % new_capacity;
+}
+
+void deque_push_front(Deque *deque, void *item)
+{
+    if (deque->size >= deque->capacity)
+        deque_grow(deque, deque->capacity * 2);
+
+    deque->front = (deque->front == 0) ? deque->capacity - 1 : deque->front - 1;
+    memcpy((char *)deque->data + deque->front * deque->item_size, item,
+           deque->item_size);
+    deque->size++;
+}
+
+void deque_push_back(Deque *deque, void *item)
+{
+    if (deque->size >= deque->capacity)
+        deque_grow(deque, deque->capacity * 2);
+
+    memcpy((char *)deque->data + deque->back * deque->item_size, item,
+           deque->item_size);
+    deque->back = (deque->back + 1) % deque->capacity;
+    deque->size++;
+}
+
+void deque_pop_front(Deque *deque)
+{
+    if (deque->size == 0)
+        return;
+
+    deque->front = (deque->front + 1) % deque->capacity;
+    deque->size--;
+}
+
+void deque_pop_back(Deque *deque)
+{
+    if (deque->size == 0)
+        return;
+
+    deque->back = (deque->back == 0) ? deque->capacity - 1 : deque->back - 1;
+    deque->size--;
+}
+
+void deque_free(Deque *deque)
+{
+    free(deque->data);
+    *deque = (Deque){ 0 };
+}
---
+++ c/void-data-structs/deque_test.c
@@ -0,0 +1,110 @@
+#include <string.h>
+#include <unity.h>
+
+#include "deque.h"
+
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+
+void test_deque_init_and_free()
+{
+    Deque deque;
+    deque_init(&deque, sizeof(int), 4);
+    TEST_ASSERT_NOT_NULL(deque.data);
+    TEST_ASSERT_EQUAL_size_t(0, deque.size);
+    TEST_ASSERT_EQUAL_size_t(4, deque.capacity);
+    TEST_ASSERT_EQUAL_size_t(sizeof(int), deque.item_size);
+    TEST_ASSERT_EQUAL_size_t(0, deque.front);
+    TEST_ASSERT_EQUAL_size_t(0, deque.back);
+
+    deque_free(&deque);
+    TEST_ASSERT_NULL(deque.data);
+    TEST_ASSERT_EQUAL_size_t(0, deque.size);
+    TEST_ASSERT_EQUAL_size_t(0, deque.capacity);
+    TEST_ASSERT_EQUAL_size_t(0, deque.item_size);
+    TEST_ASSERT_EQUAL_size_t(0, deque.front);
+    TEST_ASSERT_EQUAL_size_t(0, deque.back);
+}
+
+void test_deque_push_and_get()
+{
+    Deque deque;
+    deque_init(&deque, sizeof(int), 2);
+
+    int a = 10, b = 20;
+    deque_push_back(&deque, &a);
+    deque_push_back(&deque, &b);
+
+    TEST_ASSERT_EQUAL_size_t(2, deque.size);
+    TEST_ASSERT_EQUAL_INT(a, *(int *)deque_get(&deque, 0));
+    TEST_ASSERT_EQUAL_INT(b, *(int *)deque_get(&deque, 1));
+
+    TEST_ASSERT_NULL(deque_get(&deque, 2));
+
+    deque_free(&deque);
+}
+
+void test_deque_push_front_and_back()
+{
+    Deque deque;
+    deque_init(&deque, sizeof(int), 2);
+
+    int a = 1, b = 2, c = 3;
+    deque_push_back(&deque, &a);
+    deque_push_front(&deque, &b);
+    deque_push_back(&deque, &c);
+
+    TEST_ASSERT_EQUAL_size_t(3, deque.size);
+    TEST_ASSERT_EQUAL_INT(b, *(int *)deque_get(&deque, 0));
+    TEST_ASSERT_EQUAL_INT(a, *(int *)deque_get(&deque, 1));
+    TEST_ASSERT_EQUAL_INT(c, *(int *)deque_get(&deque, 2));
+
+    size_t old_size = deque.size;
+    deque_push_front(&deque, &b); // This should trigger a grow
+    TEST_ASSERT_EQUAL_size_t(old_size + 1, deque.size);
+    TEST_ASSERT_EQUAL_INT(b, *(int *)deque_get(&deque, 0));
+
+    deque_free(&deque);
+}
+
+void test_deque_pop_front_and_back()
+{
+    Deque deque;
+    deque_init(&deque, sizeof(int), 2);
+
+    int a = 1, b = 2, c = 3;
+    deque_push_back(&deque, &a);
+    deque_push_back(&deque, &b);
+    deque_push_back(&deque, &c);
+
+    deque_pop_front(&deque);
+    TEST_ASSERT_EQUAL_size_t(2, deque.size);
+    TEST_ASSERT_EQUAL_INT(b, *(int *)deque_get(&deque, 0));
+    TEST_ASSERT_EQUAL_INT(c, *(int *)deque_get(&deque, 1));
+
+    deque_pop_back(&deque);
+    TEST_ASSERT_EQUAL_size_t(1, deque.size);
+    TEST_ASSERT_EQUAL_INT(b, *(int *)deque_get(&deque, 0));
+
+    deque_pop_front(&deque);
+    TEST_ASSERT_EQUAL_size_t(0, deque.size);
+
+    deque_pop_back(&deque);
+    TEST_ASSERT_EQUAL_size_t(0, deque.size);
+
+    deque_free(&deque);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_deque_init_and_free);
+    RUN_TEST(test_deque_push_and_get);
+    RUN_TEST(test_deque_push_front_and_back);
+    RUN_TEST(test_deque_pop_front_and_back);
+    return UNITY_END();
+}
