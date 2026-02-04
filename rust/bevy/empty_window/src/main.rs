5c5ee03f6832a758dfdec8cc705409b23a1e3d03
Emerson MX <emersonmx@gmail.com>
2026-02-04 15:28:37 -0300

    rust/bevy: update empty_window to bevy 0.18

--- rust/bevy/empty_window/src/main.rs
+++ rust/bevy/empty_window/src/main.rs
@@ -1,17 +1,16 @@
-use bevy::{prelude::*, window::close_on_esc};
+use bevy::{prelude::*, window::WindowResolution};
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(ClearColor(Color::linear_rgb(0.0, 0.0, 0.0)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Empty Window".to_owned(),
-                resolution: (640.0, 480.0).into(),
+                resolution: WindowResolution::new(640, 480),
                 resizable: false,
                 ..default()
             }),
             ..default()
         }))
-        .add_systems(Update, close_on_esc)
         .run();
 }
