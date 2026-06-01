a500e7c97e0d8d1d95b700681884ad176f73942a
Emerson MX <emersonmx@gmail.com>
2026-06-01 09:25:52 -0300

    [c/tcpl2nd] Add section 1.2 code

---
+++ c/the_c_programming_language_2nd/exercise_1_4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// print Fahrenheit-Celsius table
+// for fahr = 0, 20, ..., 300; floating-point version
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = -20; // lower limit of temperature table
+    upper = 150; // upper limit
+    step = 10; // step size
+
+    celsius = lower;
+    printf("Celsius    Fahrenheit\n");
+    while (celsius <= upper) {
+        fahr = celsius * (9.0/5.0) + 32.0;
+        printf("%7.0f %13.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+
+    return 0;
+}
