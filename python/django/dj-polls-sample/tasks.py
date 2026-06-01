cd6d2d3d57a05eb45a46980090e7fff4a5c48fbf
Emerson MX <emersonmx@gmail.com>
2026-06-01 09:25:24 -0300

    [python/django] Remove mypy from dj-polls-sample

--- python/django/dj-polls-sample/tasks.py
+++ python/django/dj-polls-sample/tasks.py
@@ -1,6 +1,14 @@
 from invoke import Context, task
 
 
+def get_files(c: Context, file: str, exclude: str = "") -> str:
+    cmd = f"git ls-files '{file}'"
+    if exclude:
+        cmd = f"{cmd} | grep -v '{exclude}'"
+
+    return " ".join(c.run(cmd, hide=True).stdout.strip().splitlines())
+
+
 @task
 def run(c):
     # type: (Context) -> None
@@ -14,50 +22,40 @@ def shell(c):
 
 
 @task(aliases=("fmt",))
-def format(c, all_files=False):
-    # type: (Context, bool) -> None
-    precommit_options = []
+def format(c):
+    # type: (Context) -> None
+    all_files = get_files(c, ".")
+    py_files = get_files(c, "*.py")
+    ipy_files = get_files(c, "*.ipy")
 
-    if all_files:
-        precommit_options.append("--all-files")
-
-    hooks = [
-        "trailing-whitespace",
-        "end-of-file-fixer",
-        "pyupgrade",
-        "add-trailing-comma",
-        "yesqa",
-        "isort",
-        "black",
-    ]
-    for hook in hooks:
-        cmd = " ".join(["pre-commit", "run", *precommit_options, hook])
-        c.run(cmd)
+    c.run(f"trailing-whitespace-fixer {all_files}")
+    c.run(f"end-of-file-fixer {all_files}")
+    c.run(f"pyupgrade --py310-plus {py_files}")
+    c.run(f"add-trailing-comma {py_files}")
+    c.run(f"yesqa {py_files}")
+    c.run(f"isort --filter-files {py_files} {ipy_files}".strip())
+    c.run(f"black {py_files} {ipy_files}".strip())
 
 
 @task
-def lint(c, all_files=False):
-    # type: (Context, bool) -> None
-    precommit_options = []
+def lint(c):
+    # type: (Context) -> None
+    all_files = get_files(c, ".")
+    source_files = get_files(c, "*.py", "^tests/")
+    py_files = get_files(c, "*.py")
+    # ipy_files = get_files(c, "*.ipy")
+    py_test_files = get_files(c, "tests/*.py")
 
-    if all_files:
-        precommit_options.append("--all-files")
-
-    hooks = [
-        "check-ast",
-        "debug-statements",
-        "name-tests-test",
-        "check-merge-conflict",
-        "check-added-large-files",
-        "detect-private-key",
-        "flake8",
-        "mypy",
-        "vulture",
-        "bandit",
-    ]
-    for hook in hooks:
-        cmd = " ".join(["pre-commit", "run", *precommit_options, hook])
-        c.run(cmd)
+    c.run(f"check-ast {py_files}")
+    c.run(f"debug-statement-hook {py_files}")
+    c.run(f"name-tests-test {py_test_files}")
+    c.run(f"check-merge-conflict {all_files}")
+    c.run(f"check-added-large-files {all_files}")
+    c.run(f"detect-private-key {all_files}")
+    c.run(f"flake8 {py_files}")
+    # c.run(f"mypy --strict {py_files} {ipy_files}".strip())
+    c.run(f"vulture {py_files}")
+    c.run(f"bandit -r {source_files}")
 
 
 @task
